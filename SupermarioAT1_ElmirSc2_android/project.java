0fce2e81992267e3272eac115f811766900eef49
Mario Trixner <s2110824012@students.fh-hagenberg.at>
2022-05-12 12:31:29 +0200

    newOutput2

--- SupermarioAT1_ElmirSc2_android/project.java
+++ SupermarioAT1_ElmirSc2_android/project.java
@@ -1,4 +1,6 @@
-public void thisIsANewMethod() { }
+public void thisIsANewMethod() { 
+	System.out.println("Test2");
+}
 
 public static void main(String[] args) {
     System.out.println("Hello, World!");
