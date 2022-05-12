12ca08ebba31d38fa6a45289535901b6d63f87a2
elmirsoftic <elmosofti199@hotmail.com>
2022-05-12 12:30:06 +0200

    NewOutput

--- SupermarioAT1_ElmirSc2_android/project.java
+++ SupermarioAT1_ElmirSc2_android/project.java
@@ -1,5 +1,8 @@
-public void thisIsANewMethod() { }
+public void thisIsANewMethod() {
+  System.out.println("Test1");
+ }
 
 public static void main(String[] args) {
     System.out.println("Hello, World!");
+    System.out.println("Test1");
 }
